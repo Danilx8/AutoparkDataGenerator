9ffd6ea013be68638f54f43b0d660f1b8e080add
Луговских Данил Викторович <lugovskihdanil@yandex.ru>
2024-03-15 17:25:45 +0700

    complete project

---
+++ CarsGenerator.cs
@@ -0,0 +1,67 @@
+﻿using Autopark.Data;
+using Autopark.Models;
+using Bogus;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoparkDataGenerator
+{
+    public class CarsGenerator(ApplicationDbContext db)
+    {
+        protected ApplicationDbContext _db = db;
+
+        public List<Vehicle> Generate(int enterpriseId, int carsNumber)
+        {
+
+            Enterprise? enterprise = _db
+                .Enterprises
+                .Where(e => e.Id == enterpriseId)
+                .FirstOrDefault()
+                ?? throw new ArgumentException("Can't find enterprise by given id");
+
+            List<int> brands = [.. _db
+                    .Brands
+                    .Select(b => b.Id)];
+
+            List<int> drivers = [.. _db
+                    .Drivers
+                    .Where(d => d.EnterpriseId == enterpriseId)
+                    .Select(d => d.Id)];
+
+            HashSet<int> busyDrivers = [.. _db
+                .Vehicles
+                .Where(v => v.DriverId != null)
+                .Select(v => v.DriverId ?? -1)];
+
+            List<int> availableDrivers = drivers.Where(d => !busyDrivers.Contains(d)).ToList();
+
+            var vehicle = new Faker<Vehicle>("ru")
+                .RuleFor(v => v.Name, f => f.Vehicle.Manufacturer() + ' ' + f.Vehicle.Model())
+                .RuleFor(v => v.Price, f => f.Random.Int(300_000, 500_000_000))
+                .RuleFor(v => v.ZeroToHundred, f => f.Random.Float(3.0f, 50.0f))
+                .RuleFor(v => v.Mileage, f => f.Random.Int(0, 1_000_000))
+                .RuleFor(v => v.Year, f => f.Random.Int(1970, 2023))
+                .RuleFor(v => v.HorsePower, f => f.Random.Int(80, 600))
+                .RuleFor(v => v.BrandId, f => f.PickRandom(brands))
+                .RuleFor(v => v.EnterpriseId, enterpriseId)
+                .RuleFor(v => v.DriverId, f => f.PickRandom(availableDrivers).OrNull(f, .9f));
+
+            List<Vehicle> vehicles = [];
+            for (int i = 0; i < carsNumber; ++i)
+            {
+                var current = vehicle.Generate();
+                if (busyDrivers.Contains(current.DriverId ?? default)) current.DriverId = default;
+                else if (current.DriverId != null) busyDrivers.Add((int)current.DriverId);
+                vehicles.Add(current);
+            }
+
+            _db.SaveChanges();
+
+            return vehicles;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Autopark.Data;
+using Autopark.Models;
+using AutoparkDataGenerator;
+using CommandLine;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System.ComponentModel;
+
+var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;TrustServerCertificate=True")
+                .Options;
+using var _db = new ApplicationDbContext(contextOptions);
+
+Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed(o =>
+                   {
+                       CarsGenerator generator = new(_db);
+                       List<Vehicle> vehicles = generator.Generate(o.EnterpriseId, o.CarsNumber);
+                       _db.Vehicles.AddRange(vehicles);
+                       _db.SaveChanges();
+
+                       vehicles.ForEach(vehicle =>
+                       {
+                           foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(vehicle))
+                           {
+                               string name = descriptor.Name;
+                               object? value = descriptor.GetValue(vehicle);
+                               if (value?.GetType() == typeof(List<Driver>))
+                               {
+                                   Console.WriteLine($"{name}: ");
+                                   (value as List<Driver>)!.ForEach(v => Console.Write($"{v} "));
+                               }
+                               else if (value != null) Console.WriteLine("{0}={1}", name, value);
+                           }
+                       });
+                   });
+
+public class Options
+{
+    [Option('e', "enterpriseId", Required = true, HelpText = "Set vehicles' enterprise")]
+    public int EnterpriseId { get; set; }
+
+    [Option('c', "cars", Required = false, Default = 1, HelpText = "Set amount of generated vehicles. ")]
+    public int CarsNumber { get; set; }
+}
